f6b0181d524f6c0a8ea1bf6ba2d9f40be932269c
Ali Momennasab <81054872+alimomennasab@users.noreply.github.com>
2022-02-24 16:14:12 -0800

    Added array standard deviation extension

--- ViewController.swift
+++ ViewController.swift
@@ -106,3 +106,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
 }
+
+extension Array where Element: FloatingPoint {
+
+    func sum() -> Element {
+        return self.reduce(0, +)
+    }
+
+    func avg() -> Element {
+        return self.sum() / Element(self.count)
+    }
+
+    func std() -> Element {
+        let mean = self.avg()
+        let v = self.reduce(0, { $0 + ($1-mean)*($1-mean) })
+        return sqrt(v / (Element(self.count) - 1))
+    }
+}
