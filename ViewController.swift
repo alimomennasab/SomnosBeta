adfcc0f7a739aa7212cfe82bbe8c356f615d7115
Ali Momennasab <81054872+alimomennasab@users.noreply.github.com>
2022-03-03 13:05:32 -0800

    Added face tracking & lighting status function

--- ViewController.swift
+++ ViewController.swift
@@ -1,21 +1,43 @@
 import UIKit
 import SceneKit
 import ARKit
+import AVFoundation
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     //declaring view & variables
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var faceLabel: UILabel!
-        var timestamp = NSDate().timeIntervalSince1970
+    
+    
+    // MARK: Debuggers
+    @IBOutlet weak var testLabel: UILabel!
+    
+    @IBAction func resetButton(_ sender: Any) {
+        
+        //print("Reset button pressed")
+    }
+    
+    @IBAction func awakeButton(_ sender: Any) {
+        
+        print("Awake button pressed")
+    }
+    
+    @IBAction func sleepyButton(_ sender: Any) {
+        
+        print("Sleepy button presse")
+    }
+    
+    
+    var timestamp = NSDate().timeIntervalSince1970
     var queue: [Double] = []
     var analysis = ""
     var blink = false
     var acct: Float = 0.0
+    var player: AVAudioPlayer?
+    //add timer
     
     let threshold: Float = 10
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +54,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
         
         // Run the view's session
         sceneView.session.run(configuration)
@@ -68,6 +91,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     // MARK: - Checking For Blinking
     func expression(anchor: ARFaceAnchor) {
+        print(checkFaceStatus(anchor: anchor))
         //cause mirrored or smthn
         let blinkRight = anchor.blendShapes[.eyeBlinkLeft]
         let blinkLeft = anchor.blendShapes[.eyeBlinkRight]
@@ -78,9 +102,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         //blink check
         if (((blinkLeft?.decimalValue ?? 0.0 > 0.75) && (blinkRight?.decimalValue ?? 0.0 > 0.75)) && !blink){
             blink = true
-            //print("Blink: \(blink)")
             
-            //var acct = 0.0
             let newtimestamp = NSDate().timeIntervalSince1970
             let timestampDifference = abs(newtimestamp - timestamp)
             acct += Float(timestampDifference)
@@ -98,7 +120,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 if (acct > threshold){
                     acct = 0.0
                     statistics()
-                    //DO STATISTICS!!!!
                 }
             }
             
@@ -111,27 +132,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func statistics () {
         let x = 60 * 30 / queue.avg();
+        print(queue.avg())
         if(x < 10.2){
-        //sleepy (maybe have a temperary popup for now)
+            //sleepy (maybe have a temperary popup for now)
             print("You are sleepy")
         }
         if(x > 15){
-        //they need to see a doctor
+            //they need to see a doctor
             print("Seek a doctor")
         }
+    }
+    
+    func checkFaceStatus (anchor: ARFaceAnchor) -> String {
+        var status = ""
+        // move the light estimation to another
+        let frame = sceneView.session.currentFrame
+        let lightEstimate = Int (frame?.lightEstimate?.ambientIntensity ?? 0)
+        print("Lightestimate:\(lightEstimate)")
+        if lightEstimate < 100 {
+            print("Lighting is too dark")
+        } else {
+            print("Lighting good")
+        }
+        
+        status = anchor.isTracked ? "Tracking working" : "Reposition"
+        
+        return status
     }
 }
 
 extension Array where Element: FloatingPoint {
-
+    
     func sum() -> Element {
         return self.reduce(0, +)
     }
-
+    
     func avg() -> Element {
         return self.sum() / Element(self.count)
     }
-
+    
     func std() -> Element {
         let mean = self.avg()
         let v = self.reduce(0, { $0 + ($1-mean)*($1-mean) })
