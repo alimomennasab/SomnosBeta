e2a34f702ced37c7e5da805a067e075c353ae453
Ali Momennasab <81054872+alimomennasab@users.noreply.github.com>
2022-03-06 14:50:16 -0800

    Added notification function

--- ViewController.swift
+++ ViewController.swift
@@ -196,22 +196,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func playSound() {
         guard let url = Bundle.main.url(forResource: "bleep", withExtension: "mp3") else { return }
-
+        
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
             try AVAudioSession.sharedInstance().setActive(true)
-
+            
             /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
+            
             guard let player = player else { return }
-
+            
             player.play()
-
+            
         } catch let error {
             print(error.localizedDescription)
         }
     }
+}
 
 extension Array where Element: FloatingPoint {
     
